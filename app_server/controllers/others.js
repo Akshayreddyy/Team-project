21d69087028a26e321fdcde999fee95de7725d79
Akshayreddyy <akshaykarasani69@gmail.com>
2024-09-23 22:27:58 +0530

    first commit

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,9 +1,9 @@
 /* GET 'about' page */
 const about = function (req, res) {
   res.render("generic-text", {
-    title: "About Loc8r",
+    title: "About Ojas",
     content:
-      "Loc8r was created to help people find places to sit down and get a bit of work done.<br/><br/>Lorem ipsum dolor sitamet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan. Nullam sit amet interdum magna. Morbi quis faucibus nisi. Vestibulum mollis purus quis eros adipiscingtristique. Proin posuere semper tellus, id placerat augue dapibusornare. Aenean leo metus, tempus in nisl eget, accumsan interdumdui. Pellentesque sollicitudin volutpat ullamcorper.",
+      "Ojas Services was created to help people find places Where they Get Good Milk Products.<br/><br/> Great Time To Taste",
   });
 };
 module.exports = {
