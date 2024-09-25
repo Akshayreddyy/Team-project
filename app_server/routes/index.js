fde3c679ed2e16de9e50a01d1fbeb12c843b6355
Akshayreddyy <akshaykarasani69@gmail.com>
2024-09-25 23:06:54 +0530

    first commit

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -10,21 +10,4 @@ router.get('/location/review/new', ctrlLocations.addReview);
 /* Other pages */
 router.get('/about', ctrlOthers.about);
 
-const express = require('express');
-const router = express.Router();
-const { homelist, locationInfo, addReview, contactPage } = require('./controllers');
-
-// Existing routes
-router.get('/', homelist);
-router.get('/location/:id', locationInfo);
-router.get('/location/review/new', addReview);
-
-// New contact route
-router.get('/contact', contactPage);
-router.post('/contact', (req, res) => {
-  const { name, email, message } = req.body;
-  console.log(`Contact form submitted by ${name} (${email}): ${message}`);
-  res.redirect('/contact?success=true');
-});
-
 module.exports = router;
